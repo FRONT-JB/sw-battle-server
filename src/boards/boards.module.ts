c7e384d4025b863fd1b0bb2c26e0c814ab10b03d
전병길 <jeonbg@kakao.com>
2021-12-29 23:23:14 +0900

    feature : Delete Board

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,9 +3,10 @@ import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsRepository } from './boards.repository';
+import { CommentRepository } from '~/comment/comment.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardsRepository])],
+  imports: [TypeOrmModule.forFeature([BoardsRepository, CommentRepository])],
   providers: [BoardsService],
   controllers: [BoardsController],
 })
