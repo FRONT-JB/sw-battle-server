c8b00bb67829c6967f224a419f47bb558dd5d420
전병길 <jeonbg@kakao.com>
2022-01-02 20:49:19 +0900

    fix : Remove duplicates Monster

--- src/monsters/monsters.service.ts
+++ src/monsters/monsters.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { findMonster } from '~/api/monster';
+import { MonsterResult } from './model/monster.model';
 
 @Injectable()
 export class MonstersService {
   async findMonsters(name: string) {
     const searchValue = decodeURI(name).toLowerCase();
-    const { data } = await findMonster(searchValue);
-    return data;
+    const {
+      data: { count, next, previous, results },
+    } = await findMonster(searchValue);
+
+    const result: MonsterResult = {
+      count,
+      next,
+      previous,
+      results: results?.filter(
+        (monster) => monster.can_awaken === true && monster.obtainable === true,
+      ),
+    };
+
+    return result;
   }
 }
